1c1b475f5aa78d25cf6a58e314061a34f65b205e
cristianobenucci <138157662+cristianobenucci@users.noreply.github.com>
2023-07-18 11:13:18 +0200

    Update scavenging.js

--- scavenging.js
+++ scavenging.js
@@ -1,4 +1,3 @@
-function scaveng(){
 var how_long = 5;
 var leftovers = {
 	0: 0,
@@ -74,4 +73,3 @@ setTimeout(function() {
   var button = document.querySelector(`#scavenge_screen > div > div.options-container > div:nth-child(${unlocked_levels.length}) > div.status-specific > div > div.action-container > a.btn.btn-default.free_send_button`);
 button.click();
 }, Math.floor((Math.random() * 600) + 800)); 
-}
